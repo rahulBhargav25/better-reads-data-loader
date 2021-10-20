b4b6010c1d89c26a68619633448befc8225a96b9
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-21 03:20:39 +0530

    data reads operation of book reader app

---
+++ src/main/java/com/example/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,14 @@
+package com.example.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+    }
+
+}
